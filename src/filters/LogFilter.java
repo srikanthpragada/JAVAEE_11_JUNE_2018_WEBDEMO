a1986f2caade51640eecc9b7ce38b9aeb37e4ba2
srikanthpragada <srikanthpragada@gmail.com>
2018-06-27 10:27:14 +0530

    27-JUNE

---
+++ src/filters/LogFilter.java
@@ -0,0 +1,29 @@
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class LogFilter implements Filter {
+    public LogFilter() {
+        // TODO Auto-generated constructor stub
+    }
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Log Filter");		
+		chain.doFilter(request, response);
+	}
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
