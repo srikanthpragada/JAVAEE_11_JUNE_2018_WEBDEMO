b2f307403ed10d247126dcd0924c512c9d785506
srikanthpragada <srikanthpragada@gmail.com>
2018-07-11 10:00:30 +0530

    11-JULY

---
+++ src/jsf/JobsBean.java
@@ -0,0 +1,38 @@
+package jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.sql.rowset.CachedRowSet;
+
+import oracle.jdbc.rowset.OracleCachedRowSet;
+
+@ManagedBean
+public class JobsBean {
+	
+	public  List<Job> getJobs() {
+	
+		try (CachedRowSet crs = new OracleCachedRowSet()) {
+			crs.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+			crs.setUsername("hr");
+			crs.setPassword("hr");
+			crs.setCommand("select * from jobs");
+			crs.execute();
+			ArrayList<Job> jobs = new ArrayList<>();
+			while (crs.next()) {
+				Job j = new Job();
+				j.setId( crs.getString(1));
+				j.setTitle( crs.getString(2));
+				jobs.add(j);
+			}
+			return jobs;
+		}
+		catch(Exception ex) {
+			System.out.println(ex);
+			return null;
+		}
+
+	}
+
+}
